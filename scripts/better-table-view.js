0d86e20f93b2676bcef886cf7c0a9361cc708c3c
Samir Hadi <ultrakorne@gmail.com>
2020-05-11 01:47:26 +0200

    cleaning up code

--- scripts/better-table-view.js
+++ scripts/better-table-view.js
@@ -13,13 +13,12 @@ export class BetterRT {
 
         let tableViewClass = html[0].getElementsByClassName(tableClassName)[0];
 
-        //re-renders
+        //re-renders, without this the view has a scroll bar and its not sized correctly
         if (tableViewClass) {
             html[0].style.display = 'none';
             html[0].style.display = 'block';
         }
 
-        // console.log("tableViewClass ", htmlclass[0].getElementsByClassName("editable"));
         if (!tableViewClass) { //when the table is updated, the html is different
             if (html[0].getAttribute("class") === tableClassName) {
                 tableViewClass = html[0];
@@ -29,43 +28,42 @@ export class BetterRT {
         }
 
         let divBetterTableType = document.createElement("div");
-        // divBetterTableType.setAttribute("class", "form-group");
-
         let selectTypeHtml = await renderTemplate("modules/better-rolltables/templates/select-table-type.html", tableEntity);
         divBetterTableType.innerHTML = selectTypeHtml;
-
         tableViewClass.insertBefore(divBetterTableType, tableViewClass.children[2]);
 
         const selectTypeElement = divBetterTableType.getElementsByTagName("select")[0];
-
-        const allInputs = divBetterTableType.getElementsByTagName("input");
-        const currencyInput = allInputs.namedItem("currency-input");
-
-
-        if (currencyInput) {
-            console.log("currencyInput value ", currencyInput.value);
-            currencyInput.oninput = async function () { await BetterRT.onCurrencyInput(currencyInput.value, tableEntity); };
-        }
-
-        // selectTypeElement.value = selectedTableType;
         selectTypeElement.onchange = async function () { await BetterRT.onOptionTypeChanged(selectTypeElement.value, tableEntity); };
 
+        BetterRT.configureCurrencyInputField(divBetterTableType);
 
         //create generate loot button
         if (selectedTableType === "loot") {
             const footer = html[0].getElementsByClassName("sheet-footer flexrow")[0];
             console.log("footer ", footer);
-
-            let generateLootBtn;
-            console.log("adding button");
-            generateLootBtn = document.createElement("button");
-            generateLootBtn.setAttribute("class", "generate");
-            generateLootBtn.setAttribute("type", "button");
-
-            generateLootBtn.innerHTML = `<i id="BRT-gen-loot" class="fas fa-coins"></i> ${i18n('BRT.GenerateLoot.Button')}`;
-            generateLootBtn.onclick = () => { BetterRT.generateLoot(tableEntity); };
-            footer.insertBefore(generateLootBtn, footer.firstChild);
+            BetterRT.showGenerateLootButton(footer);
         }
+    }
+
+    //if the currency-input exist (when selectedTableType === "loot" but configured in the handlebars rendered html) we configure the field
+    static async configureCurrencyInputField(htmlElement) {
+        const allInputs = htmlElement.getElementsByTagName("input");
+        const currencyInput = allInputs.namedItem("currency-input");
+
+        if (!currencyInput) return;
+
+        console.log("currencyInput value ", currencyInput.value);
+        currencyInput.oninput = async function () { await BetterRT.onCurrencyInput(currencyInput.value, tableEntity); };
+    }
+
+    static async showGenerateLootButton(htmlElement) {
+        const generateLootBtn = document.createElement("button");
+        generateLootBtn.setAttribute("class", "generate");
+        generateLootBtn.setAttribute("type", "button");
+
+        generateLootBtn.innerHTML = `<i id="BRT-gen-loot" class="fas fa-coins"></i> ${i18n('BRT.GenerateLoot.Button')}`;
+        generateLootBtn.onclick = () => { BetterRT.generateLoot(tableEntity); };
+        htmlElement.insertBefore(generateLootBtn, htmlElement.firstChild);
     }
 
     static generateLoot(tableEntity) {
@@ -74,20 +72,12 @@ export class BetterRT {
 
     // game.tables.getName("Your Roll Table Name Here")
     static async onOptionTypeChanged(value, tableEntity) {
-        // console.log("Value ", value, " . Entity: ", tableId);
-
         // const table = game.tables.entities.find(t => t.id === tableId);
         console.log("entity table ", tableEntity);
-
-        // let updates = [];
-        // updates.push({ "_id": tableId, "data.flags.better-rolltables.table-type": value })
-        // await table.update(updates);
-
         await tableEntity.setFlag("better-rolltables", "table-type", value);
     }
 
     static async onCurrencyInput(value, tableEntity) {
         console.log("onCurrencyInput table ", tableEntity);
-
     }
 }

f8ec114519461e77c2f9f1fb51a180f677822c6c
Samir Hadi <samir@chatterbox.games>
2020-06-06 18:39:59 +0200

    extracting definition and value from the story definition

---
+++ scripts/story/story-bool-condition.js
@@ -0,0 +1,19 @@
+/*
+gender == "male" AND race == "human" OR x != "bla"
+
+S:: = Expression
+Expression:: = SubExpression
+             | SubExpression ' AND ' Expression
+             | SubExpression ' OR ' Expression
+SubExpression:: = Definition '==' Value
+                | Definition '!=' Value
+Definition:: = [a-zA-Z]
+Value:: = '"' [a-zA-Z] '"'
+*/
+
+export class StoryBoolCondition {
+
+    evaluate() {
+        return true;
+    }
+}
--- scripts/story/story-builder.js
+++ scripts/story/story-builder.js
@@ -1,7 +1,13 @@
+import { StoryBoolCondition } from './story-bool-condition.js';
+
 export class StoryBuilder {
 
     constructor(tableEntity) {
         this.table = tableEntity;
+        /** the story tokens with the respective values, either pulled from a rolltable or rolled with a formula */
+        this.storyTokens = {};
+        /** string containing the story, to be replaced with the tokens */
+        this.story = "";
     }
 
     async drawStory() {
@@ -12,14 +18,17 @@ export class StoryBuilder {
             if (entry.collection == "JournalEntry") {
 
                 const storyJournal = game.journal.get(entry.resultId);
-                this.parseStoryDefinition(storyJournal.data.content);
+                await this.parseStoryDefinition(storyJournal.data.content);
             } else {
                 ui.notifications.warn(`Entry for ${entry.compendium} not supported for type story`);
             }
         }
+
+        console.log("this.storyTokens ", this.storyTokens);
+        console.log("story ", this.story);
     }
 
-    parseStoryDefinition(storyDefinition) {
+    async parseStoryDefinition(storyDefinition) {
 
         const PARSE_MODE = {
             NONE: 0,
@@ -36,35 +45,99 @@ export class StoryBuilder {
         for (const line of lines) {
             // console.log("story line ", line);
             switch (parseMode) {
+                case PARSE_MODE.STORY:
+                    this.story += line;
+                    break;
                 case PARSE_MODE.DEFINITION:
-                    let input = line;
-                    let regex = /\s*<p>(.+)AS(.+)<\/p>/g;
-                    let matches;
-                    while (matches = regex.exec(input)) {
-
-                        this.processDefinition(matches[1], matches[2]);
-
+                    const matches = /\s*<p>(.+)AS(.+)<\/p>/.exec(line);
+                    if (matches) {
+                        await this.processDefinition(matches[1], matches[2]);
+                        break;
+                    }
+                default:
+                    const sectionMatch = /.*#([a-zA-Z]+)/.exec(line);
+                    if (sectionMatch && sectionMatch[1] == "story") {
+                        parseMode = PARSE_MODE.STORY;
                     }
                     break;
             }
-
         }
     }
 
-    processDefinition(defValue, definitionName) {
 
-        console.log("value ", defValue);
+    async processDefinition(defValue, definitionName) {
+
+        // console.log("value ", defValue);
 
         const match = /{ *([^}]*?) *}/.exec(definitionName);
-        if(!match) {
-            ui.notification.error(`definition error, ${definitionName} is malformed. After keyword AS we expect a name in brackets {}`)
+        if (!match) {
+            ui.notifications.error(`definition error, ${definitionName} is malformed. After keyword AS we expect a name in brackets {}`)
             return;
         }
         const definition = match[1];
-        console.log("definition ", definition);
+        if (hasProperty(this.storyTokens, definition)) {
+            console.log(`definition ${definition} is already defined, skipping line`);
+            return;
+        }
+
+        // console.log("definition ", definition);
 
         const regexIF = /IF\s*\((.+)\)/;
+
+        const ifMatch = regexIF.exec(defValue);
+        let conditionMet = true;
+        if (ifMatch) {
+            const storyCondition = new StoryBoolCondition(defValue);
+            conditionMet = storyCondition.evaluate();
+        }
+
+        if (!conditionMet) return;
+
         const regexTable = /\s*@RollTable\[ *([^\]]*?) *\]{ *([^}]*?) *}/;
-        const regexRoll = /\s*\[\[ *([^\]]*?) *\]\]/;
+
+        const tableMatch = regexTable.exec(defValue);
+        let valueResult;
+        /** there is a table definition on the left of the AS */
+        if (tableMatch) {
+            const tableId = tableMatch[1];
+            const table = game.tables.get(tableId);
+            // console.log("table id ", tableId);
+            const draw = await table.drawMany(1, { displayChat: false });
+            if (!draw) {
+                await table.reset();
+                draw = await table.drawMany(1, { displayChat: false });
+            }
+
+            if (draw.results.length != 1) {
+                ui.notifications.error(`0 or more than 1 result was drawn from table ${table.name}, only 1 result is supported check your table config`);
+                return;
+            }
+
+            const tableResult = draw.results[0];
+            if (tableResult.type != 0) {
+                ui.notifications.warn(`only text result from table are supported at the moment, check table ${table.name}`);
+            }
+            valueResult = tableResult.text;
+
+        } else {
+            const regexRoll = /\s*\[\[ *([^\]]*?) *\]\]/;
+            /** if no table match, lets check for a formula */
+            const rollMatch = regexRoll.exec(defValue)
+            if (rollMatch) {
+                const rollFormula = rollMatch[1];
+                // console.log("roll formula ", rollFormula);
+                try {
+                    valueResult = new Roll(rollFormula).roll().total || 0;
+                } catch (error) {
+                    valueResult = 0;
+                }
+            } else {
+                ui.notifications.error(`on the left side of the AS in a story definition a rolltable or rollformula must be provided`);
+            }
+        }
+
+        if (valueResult) {
+            setProperty(this.storyTokens, definition, valueResult);
+        }
     }
 }

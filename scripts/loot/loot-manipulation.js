a48d7c161ff6e22b88b8a2dd76450974da943161
Daimakaicho <cbouyer@protonmail.com>
2021-07-21 13:26:11 +0200

    linted code

--- scripts/loot/loot-manipulation.js
+++ scripts/loot/loot-manipulation.js
@@ -1,46 +1,44 @@
-import { BRTCONFIG } from '../core/config.js';
-import { findInCompendiumById } from "../core/utils.js";
+import { findInCompendiumById } from '../core/utils.js'
 
 export class LootManipulator {
+  async _getRandomSpell (level) {
+    const spells = game.betterTables.getSpellCache().filter(spell => spell.data.level === level)
+    const randomIndex = Math.floor(Math.random() * spells.length)
+    const spell = spells[randomIndex]
+    return findInCompendiumById(spell.collection, spell._id)
+  }
 
-    async _getRandomSpell(level) {
-        const spells = game.betterTables.getSpellCache().filter(spell => spell.data.level === level);
-        const randomIndex = Math.floor(Math.random() * spells.length);
-        const spell = spells[randomIndex];
-        return findInCompendiumById(spell.collection, spell._id);
+  async preItemCreationDataManipulation (itemData) {
+    // we duplicate item now in order to modify it
+    itemData = duplicate(itemData)
+
+    // const match = BRTCONFIG.SCROLL_REGEX.exec(itemData.name);
+    let match = /\s*Spell\s*Scroll\s*(\d+|cantrip)/gi.exec(itemData.name)
+
+    if (!match) {
+      // pf2e temporary FIXME add this in a proper config
+      match = /\s*Scroll\s*of\s*(\d+)/gi.exec(itemData.name)
     }
 
-    async preItemCreationDataManipulation(itemData) {
-        // we duplicate item now in order to modify it
-        itemData = duplicate(itemData);
-
-        // const match = BRTCONFIG.SCROLL_REGEX.exec(itemData.name);
-        let match = /\s*Spell\s*Scroll\s*(\d+|cantrip)/gi.exec(itemData.name);
-
-        if (!match) {
-            //pf2e temporary FIXME add this in a proper config
-            match = /\s*Scroll\s*of\s*(\d+)/gi.exec(itemData.name);
-        }
-
-        if (!match) {
-            return itemData;
-        }
-
-        //if its a scorll then open compendium
-        let level = match[1].toLowerCase() === "cantrip" ? 0 : parseInt(match[1]);
-        const itemEntity = await this._getRandomSpell(level);
-
-        if (!itemEntity) {
-            ui.notifications.warn(`no spell of level ${level} found in compendium  ${itemEntity.collection} `);
-            return itemData;
-        }
-
-        let itemLink = `@Compendium[${itemEntity.collection}.${itemEntity.data._id}]`;
-        //make the name shorter by removing some text
-        itemData.name = itemData.name.replace(/^(Spell\s)/, "");
-        itemData.name = itemData.name.replace(/(Cantrip\sLevel)/, "Cantrip");
-        itemData.name += ` (${itemEntity.data.name})`;
-        itemData.data.description.value = '<blockquote>' + itemLink + '<br />'+ itemEntity.data.data.description.value + '<hr />' + itemData.data.description.value + '</blockquote>';
-        return itemData;
+    if (!match) {
+      return itemData
     }
+
+    // if its a scorll then open compendium
+    const level = match[1].toLowerCase() === 'cantrip' ? 0 : parseInt(match[1])
+    const itemEntity = await this._getRandomSpell(level)
+
+    if (!itemEntity) {
+      ui.notifications.warn(`no spell of level ${level} found in compendium  ${itemEntity.collection} `)
+      return itemData
+    }
+
+    const itemLink = `@Compendium[${itemEntity.collection}.${itemEntity.data._id}]`
+    // make the name shorter by removing some text
+    itemData.name = itemData.name.replace(/^(Spell\s)/, '')
+    itemData.name = itemData.name.replace(/(Cantrip\sLevel)/, 'Cantrip')
+    itemData.name += ` (${itemEntity.data.name})`
+    itemData.data.description.value = '<blockquote>' + itemLink + '<br />' + itemEntity.data.data.description.value + '<hr />' + itemData.data.description.value + '</blockquote>'
+    return itemData
+  }
 }
--- scripts/story/story-chat-card.js
+++ scripts/story/story-chat-card.js
@@ -1,33 +1,33 @@
-import { addRollModeToChatData } from '../core/utils.js';
+import { addRollModeToChatData } from '../core/utils.js'
 
 export class StoryChatCard {
-    constructor(tableEntity) {
-        this._tableEntity = tableEntity;
-    }
+  constructor (tableEntity) {
+    this._tableEntity = tableEntity
+  }
 
-    /**
+  /**
      * Create a chat card to display the story string
      * @param {string} story the html string of the story to display in chat
-     * @param {dictionary} options set of options, if gmOnly = true then the card will be set to shown only to GM regardless of the chat preferences
+     * @param {Object} options set of options, if gmOnly = true then the card will be set to shown only to GM regardless of the chat preferences
      */
-    createChatCard(story, options = {}) {
-        if (!story) return;
+  createChatCard (story, options = {}) {
+    if (!story) return
 
-        //quickfix for textselection of stories
-        story = '<div class="story-text-selectable">' + story + "</div>";
-        
-        let chatData = {
-            flavor: this._tableEntity.data.name,
-            sound: "sounds/dice.wav",
-            user: game.user._id,
-            content: story
-        }
-        if (options.gmOnly) {
-            chatData.whisper = [game.users.find(u => u.isGM).id];
-        } else {
-            addRollModeToChatData(chatData);
-        }
+    // quickfix for textselection of stories
+    story = '<div class="story-text-selectable">' + story + '</div>'
 
-        ChatMessage.create(chatData);
+    const chatData = {
+      flavor: this._tableEntity.data.name,
+      sound: 'sounds/dice.wav',
+      user: game.user._id,
+      content: story
     }
+    if (options.gmOnly) {
+      chatData.whisper = [game.users.find(u => u.isGM).id]
+    } else {
+      addRollModeToChatData(chatData)
+    }
+
+    ChatMessage.create(chatData)
+  }
 }
